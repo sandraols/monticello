e9139e85e0f0358702b19d1d16fbc41a77b4d7c5
sandraols <sandra.helene.olsson@gmail.com>
2020-05-15 13:33:24 +0200

    add map section

--- assets/src/script.js
+++ assets/src/script.js
@@ -9,20 +9,23 @@ $('.news__slider').slick({
   });
 
 $('.left').click(function(){
-$('.slider').slick('slickPrev');
+$('.news__slider').slick('slickPrev');
 })
 
 $('.right').click(function(){
-$('.slider').slick('slickNext');
+$('.news__slider').slick('slickNext');
 })
 
 
 function initMap() {
-    const london = {lat: 51.506707, lng: -0.125959};
+    const london = {lat: 51.506707, lng: -0.125963};
     const map = new google.maps.Map(
         document.getElementById('map'), {
             zoom: 15, 
             center: london,
+            gestureHandling: 'none',
+            zoomControl: false,
+            disableDefaultUI: true,
             styles: [
                 {
                 "elementType": "geometry",
@@ -186,9 +189,29 @@ function initMap() {
     });
 
     let marker = new google.maps.Marker({
-        position: london, 
+        position: {lat: 51.510994, lng: -0.129903},
         map: map,
         icon: "assets/images/marker.png",
     });
-
 }
+
+
+// const form  = document.getElementsByTagName('form')[0];
+const email = document.getElementById("email");
+// const name = document.getElementById("name");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("Please fill in a valid email adress");
+  } else {
+    email.setCustomValidity("");
+  }
+});
+
+// name.addEventListener("input", function (event) {
+//     if (name.validity.typeMismatch) {
+//       name.setCustomValidity("I am expecting a name");
+//     } else {
+//       name.setCustomValidity("");
+//     }
+//   });
